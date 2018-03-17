f29099d32f5d37757a50b1bf861b7b2adfd6ba11
wassim <ouali.wssim@hotmail.com>
2018-03-17 15:06:32 +0100

    Modification du HTML du site

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -6,5 +6,6 @@ urlpatterns = [
     path('base', views.acc),
     path('date', views.date_actuelle),
     path('addition/<int:nombre1>/<int:nombre2>/',views.addition),
+    url(r'^$', views.post_list, name='post_list'),
     
     ]
